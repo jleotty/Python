b071d1728edea847d9804aeb9f33f191b888122c
Julian VSCODE <julianleotty@gmail.com>
2023-09-14 02:20:08 -0300

    upgrade__

--- Envio de report no Slack.py
+++ Envio de report no Slack.py
@@ -3,110 +3,127 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import pyautogui
 import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-# Inicializar o navegador
-service = Service(ChromeDriverManager().install())
-navegador = webdriver.Chrome(service=service)
+try:
+    # Inicializar o navegador
+    service = Service(ChromeDriverManager().install())
+    navegador = webdriver.Chrome(service=service)
 
-# acessar power bi via web
-navegador.get('adicionar URL')
-time.sleep(7)
+    # acessar power bi via web
+    navegador.get('Inserir URL')
+    time.sleep(7)
 
-# maximar navegador
-pyautogui.hotkey('win', 'up')
-time.sleep(1)
+    # maximar navegador
+    pyautogui.hotkey('win', 'up')
+    time.sleep(1)
 
+    click = navegador.find_element('id', 'email')
+    click.click()
+    time.sleep(2)
 
-click = navegador.find_element('id', 'email')
-click.click()
-time.sleep(2)
+    # inserir e-mail
+    pyautogui.write('x856594@gruposantander.com')
+    pyautogui.press('enter')
+    time.sleep(15)
 
-# inserir e-mail
-pyautogui.write('e-mail')
-pyautogui.press('enter')
+    # inserir usuário novamente
+    pyautogui.write('x856594')
+    time.sleep(3)
 
-time.sleep(8)
-# inserir usuário novamente
-pyautogui.write('usuário')
-time.sleep(3)
+    # Ir para o campo abaixo para que seja possível inserir a senha
+    pyautogui.press('tab')
+    time.sleep(3)
 
-# Ir para o campo abaixo para que seja possível inserir a senha
-pyautogui.press('tab')
-time.sleep(3)
+    # inserir senha
+    pyautogui.write('Pago@@@2023')
+    pyautogui.press('enter')
+    time.sleep(20)
 
-# inserir senha
-pyautogui.write('senha')
-pyautogui.press('enter')
-time.sleep(25)
+    # Pressionar "printscreen"
+    pyautogui.press('printscreen')
+    time.sleep(4)
 
-# Pressionar "printscreen"
-pyautogui.press('printscreen')
-time.sleep(6)
+    # Defina as coordenadas iniciais e finais para a seleção
+    x_inicial, y_inicial = 164, 143
+    x_final, y_final = 1197, 700
 
-# Defina as coordenadas iniciais e finais para a seleção
-x_inicial, y_inicial = 355, 214
-x_final, y_final = 1230, 700
+    # Ajuste para dar tempo de você mover o mouse para a posição correta
+    time.sleep(5)
 
-# Ajuste para dar tempo de você mover o mouse para a posição correta
-time.sleep(5)
+    # Move o mouse para a posição inicial
+    pyautogui.moveTo(x_inicial, y_inicial)
 
-# Move o mouse para a posição inicial
-pyautogui.moveTo(x_inicial, y_inicial)
+    # Mantém o botão do mouse pressionado
+    pyautogui.mouseDown()
 
-# Mantém o botão do mouse pressionado
-pyautogui.mouseDown()
+    # Move o mouse para a posição final
+    pyautogui.moveTo(x_final, y_final)
 
-# Move o mouse para a posição final
-pyautogui.moveTo(x_final, y_final)
+    # Solta o botão do mouse
+    pyautogui.mouseUp()
 
-# Solta o botão do mouse
-pyautogui.mouseUp()
+    # selecionar a opção de copiar print
+    time.sleep(1)
+    pyautogui.write('c')
+    time.sleep(2)
+    pyautogui.press('enter')
+    time.sleep(3)
 
-# selecionar a opção de copiar print
-pyautogui.press('up')
-time.sleep(3)
+    # Encerrar navegador
+    navegador.quit()
 
-pyautogui.press('up')
-time.sleep(3)
+    # iniciar aplicação do slack
+    
+    pyautogui.hotkey('win', 'r')
+    time.sleep(3)
+    pyautogui.write(r'C:\Users\julian.dose\AppData\Local\slack\slack.exe')
+    time.sleep(15)
+    pyautogui.press('enter')
+    time.sleep(4)
+    pyautogui.hotkey('win', 'up')
 
-pyautogui.press('up')
-time.sleep(3)
+    # Colar print
+    pyautogui.hotkey('ctrl', 'v')
+    time.sleep(2)
+    pyautogui.press('enter')
+    time.sleep(4)
 
-pyautogui.press('up')
-time.sleep(3)
+    # fechar Slack
+    pyautogui.click(1338, 14)
 
-pyautogui.press('up')
-time.sleep(3)
+except Exception as ex:
+    # Se ocorrer uma exceção, envie um e-mail de notificação e registre no arquivo de log
+    error_message = "Ocorreu um erro durante a execução do código: " + str(ex)
+    print(error_message)
+   
 
-pyautogui.press('enter')
-time.sleep(3)
+    # Configurações do e-mail de notificação
+    smtp_server = 'smtp.office365.com'
+    smtp_port = 587  # Porta padrão para TLS
+    email_from = 'E-MAIL'  # Seu endereço de e-mail
+    email_password = 'SENHA'  # Sua senha de e-mail
+    email_to = 'E-MAIL DO DESTINATARIO'  # Endereço de e-mail do destinatário
 
-# Encerrar navegador
+    # Conteúdo do e-mail de notificação
+    subject = "Erro na execução do script"
+    email_body = "Ocorreu um erro durante a execução do script envio do Report, realizar validação manualmente e verificar script!"
 
-navegador.quit()
+    # Criar o objeto MIMEText
+    msg = MIMEMultipart()
+    msg.attach(MIMEText(email_body, 'plain'))
+    msg['Subject'] = subject
+    msg['From'] = email_from
+    msg['To'] = email_to
 
-# iniciar aplicação do slack
-pyautogui.press('win')
-time.sleep(3)
-
-pyautogui.write('Slack')
-time.sleep(2)
-
-pyautogui.press('enter')
-time.sleep(21)
-
-# Colar print
-pyautogui.hotkey('ctrl', 'v')
-time.sleep(2)
-
-pyautogui.press('enter')
-
-time.sleep(4)
-
-pyautogui.hotkey('win', 'r')
-time.sleep(3)
-
-# fechar Slack
-pyautogui.write('taskkill /f /im Slack.exe')
-time.sleep(2)
-pyautogui.press('enter')
+    try:
+        # Enviar o e-mail de notificação via SMTP do Outlook
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(email_from, email_password)
+            server.sendmail(email_from, email_to, msg.as_string())
+        print("E-mail de notificação enviado!")
+    except Exception as e:
+        print("Ocorreu um erro ao enviar o e-mail de notificação:", str(e))
